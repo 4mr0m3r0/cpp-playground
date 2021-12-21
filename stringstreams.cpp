d8a4523c83cfb3ae5fb0efa922997bccecaf95e4
Angel <4mr0m3r0@gmail.com>
2021-12-21 09:52:11 -0300

    adding stringstreams

---
+++ stringstreams.cpp
@@ -0,0 +1,27 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+    stringstream ss(str);
+    vector<int> numbers;
+    int current_number;
+    char ch;
+    while (ss >> current_number) {
+        numbers.push_back(current_number);
+        ss >> ch;
+    }
+    return numbers;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
